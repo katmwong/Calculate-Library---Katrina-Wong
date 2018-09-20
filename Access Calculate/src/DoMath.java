32f000c93b00f2cc875103356e7fbcba070e2cb3
copykaaat <copykaaat@192.168.1.3>
2018-09-19 22:46:12 -0700

    h

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -2,11 +2,10 @@
  * @Katrina Wong
  * @Version September 6, 2018
  */
-public clasns DoMath {
+public class DoMath {
 
 	public static void main(String[] args){
-		System.out.println(Calculate.exponent(2,3));
-
+		System.out.println(Calculate.isPrime(14));
 	}
 
 }
